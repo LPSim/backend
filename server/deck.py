e1a621ee35a3276bc97cc7d01e7c8eb6554ae243
zyr17 <jzjqz17@gmail.com>
2023-08-27 00:26:01 +0800

    feat: support string input to generate deck

--- server/deck.py
+++ server/deck.py
@@ -1,6 +1,6 @@
 import logging
 
-from utils import BaseModel
+from utils import BaseModel, get_instance_from_type_unions
 from typing import Literal, List
 from . import Cards
 from . import Charactors
@@ -39,3 +39,42 @@ class Deck(BaseModel):
                     )
                     return False
         return True
+
+    @staticmethod
+    def from_str(deck_str: str) -> 'Deck':
+        """
+        Convert deck string to deck object. One line contains one card or
+        charactor. If declare a charactor, use 'charactor:'. Otherwise 
+        write card name directly. To declare multiple same cards or charactors,
+        add `*number` after the line. For example:
+
+        charactor:Fischl*2
+        charactor:PyroMobMage
+        Strategize*15
+        Stellar Predator*15
+
+        """
+        deck_str = deck_str.strip()
+        deck = Deck()
+        for line in deck_str.split('\n'):
+            line = line.strip()
+            if '*' in line:
+                line, number = line.split('*')
+                number = int(number)
+            else:
+                number = 1
+            if line.startswith('charactor:'):
+                for _ in range(number):
+                    deck.charactors.append(
+                        get_instance_from_type_unions(
+                            Charactors, { 'name': line[10:] }
+                        )
+                    )
+            else:
+                for _ in range(number):
+                    deck.cards.append(
+                        get_instance_from_type_unions(
+                            Cards, { 'name': line }
+                        )
+                    )
+        return deck
