36fe7bb35a4cc14c1d468dd5aba03ee47fcff12c
zyr17 <jzjqz17@gmail.com>
2023-09-03 18:21:39 +0800

    feat: implement liben

--- server/card/support/companions.py
+++ server/card/support/companions.py
@@ -1,18 +1,20 @@
 from typing import Any, Literal, List
 
+from ...dice import Dice
+
 from .base import SupportBase
 from ...consts import (
-    CostLabels, DieColor, ElementType, ELEMENT_TO_DIE_COLOR, 
-    ObjectPositionType, SkillType
+    ELEMENT_DEFAULT_ORDER, CostLabels, DieColor, ElementType, 
+    ELEMENT_TO_DIE_COLOR, ObjectPositionType, SkillType
 )
 from ...struct import Cost
 from ...action import (
     Actions, ChangeObjectUsageAction, CreateDiceAction, DrawCardAction, 
-    RemoveObjectAction
+    RemoveDiceAction, RemoveObjectAction
 )
 from ...event import (
-    ChangeObjectUsageEventArguments, RoundPrepareEventArguments, 
-    SkillEndEventArguments
+    ChangeObjectUsageEventArguments, RoundEndEventArguments, 
+    RoundPrepareEventArguments, SkillEndEventArguments
 )
 
 
@@ -92,6 +94,79 @@ class Timmie(CompanionBase):
         return ret
 
 
+class Liben(CompanionBase):
+    name: Literal['Liben']
+    desc: str = (
+        'End Phase: Collect your unused Elemental Dice (Max 1 of each '
+        'Elemental Type). '
+        'When Action Phase begins: If this card has collected 3 Elemental '
+        'Dice, draw 2 cards and create Omni Element x2, then discard this '
+        'card.'
+    )
+    version: Literal['3.3'] = '3.3'
+    cost: Cost = Cost()
+    usage: int = 0
+    max_usage: int = 3
+
+    def event_handler_ROUND_END(
+        self, event: RoundEndEventArguments, match: Any
+    ) -> List[RemoveDiceAction]:
+        """
+        Collect different color dice in round end.
+        """
+        if self.position.area != ObjectPositionType.SUPPORT:
+            # not in support area, do nothing
+            return []
+        collect_order: List[DieColor] = []
+        dice: Dice = match.player_tables[self.position.player_idx].dice
+        colors = dice.colors
+        # collect die in element default order
+        for element in ELEMENT_DEFAULT_ORDER:
+            color = ELEMENT_TO_DIE_COLOR[element]
+            if color in colors:
+                collect_order.append(color)
+        # collect OMNI at last
+        for color in colors:
+            if color == DieColor.OMNI:
+                collect_order.append(color)
+        # only collect dice if usage is not full
+        collect_order = collect_order[: self.max_usage - self.usage]
+        self.usage += len(collect_order)
+        return [RemoveDiceAction(
+            player_idx = self.position.player_idx,
+            dice_idxs = dice.colors_to_idx(collect_order)
+        )]
+
+    def event_handler_ROUND_PREPARE(
+        self, event: RoundPrepareEventArguments, match: Any
+    ) -> List[CreateDiceAction | DrawCardAction | RemoveObjectAction]:
+        """
+        If this card has collected 3 Elemental Dice, draw 2 cards and create 
+        Omni Element x2, then discard this card.'
+        """
+        if self.position.area != ObjectPositionType.SUPPORT:
+            # not in support area, do nothing
+            return []
+        if self.usage != self.max_usage:
+            # not enough dice collected
+            return []
+        return [ 
+            CreateDiceAction(
+                player_idx = self.position.player_idx,
+                number = 2,
+                color = DieColor.OMNI,
+            ),
+            DrawCardAction(
+                player_idx = self.position.player_idx,
+                number = 2,
+                draw_if_filtered_not_enough = True
+            ),
+            RemoveObjectAction(
+                object_position = self.position,
+            ),
+        ]
+
+
 class Rana(CompanionBase):
     name: Literal['Rana']
     desc: str = (
@@ -147,4 +222,4 @@ class Rana(CompanionBase):
         return []
 
 
-Companions = Timmie | Rana
+Companions = Timmie | Liben | Rana
