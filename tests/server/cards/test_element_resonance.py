9f3c5bdeb62f5c1d81530ef00095b43eaf0a8179
zyr17 <jzjqz17@gmail.com>
2023-09-03 21:44:36 +0800

    test: enduring rock

--- tests/server/cards/test_element_resonance.py
+++ tests/server/cards/test_element_resonance.py
@@ -2,7 +2,7 @@ from agents.interaction_agent import InteractionAgent
 from server.match import Match, MatchState
 from server.deck import Deck
 from tests.utils_for_test import (
-    get_random_state, get_test_id_from_command, make_respond, 
+    check_hp, get_random_state, get_test_id_from_command, make_respond, 
     set_16_omni
 )
 
@@ -121,5 +121,142 @@ def test_woven_element():
     assert match.state != MatchState.ERROR
 
 
+def test_enduring_rock():
+    cmd_records = [
+        [
+            "sw_card",
+            "choose 2",
+            "skill 1 0 1 2",
+            "card 2 0 0",
+            "skill 0 0 1 2",
+            "TEST 2 p1 5 full plate",
+            "sw_char 0 0",
+            "skill 0 0 1 2",
+            "sw_char 2 0",
+            "skill 2 0 1 2 3",
+            "TEST 1 9 10 10 9 10 7",
+            "TEST 3 p0 4 crystallize",
+            "end",
+            "card 3 0 0",
+            "skill 0 0 1",
+            "end",
+            "end",
+            "end",
+            "card 3 0 0",
+            "sw_char 1 0",
+            "skill 1 0 1 2",
+            "TEST 3 p0 1 elemental resonance: enduring rock",
+            "end"
+        ],
+        [
+            "sw_card 1 2 3",
+            "choose 2",
+            "card 0 0 0",
+            "skill 0 0 1 2",
+            "TEST 4 p1 1 team status",
+            "TEST 5 p0 1 full plate 1 geo",
+            "skill 1 0 1 2",
+            "sw_char 0 0",
+            "skill 0 0 1 2",
+            "end",
+            "skill 0 0 1 2",
+            "TEST 3 p0 6 crystallize",
+            "card 0 0 0",
+            "card 4 0 0",
+            "sw_char 1 0",
+            "card 0 0 0 1 2",
+            "skill 0 0 1 2",
+            "TEST 6 p1 1 rebellious shield",
+            "end",
+            "end",
+            "card 5 0 0",
+            "skill 1 0 1 2",
+            "TEST 6 p1 5 rebellious shield",
+            "end",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:PyroMobMage
+        charactor:Arataki Itto
+        charactor:Noelle
+        Elemental Resonance: Enduring Rock*15
+        The Bell*15
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                hps = [int(x) for x in cmd.split(' ')[2:]]
+                hps = [hps[:3], hps[3:]]
+                check_hp(match, hps)
+            elif test_id in [2, 3, 6]:
+                cmd = cmd.split(' ')
+                pid = int(cmd[2][1])
+                usage = int(cmd[3])
+                found = False
+                for status in match.player_tables[pid].team_status:
+                    if status.name.lower() == ' '.join(cmd[4:]):
+                        assert status.usage == usage
+                        found = True
+                assert found
+            elif test_id == 4:
+                assert len(match.player_tables[1].team_status) == 1
+            elif test_id == 5:
+                assert len(match.player_tables[0].team_status) == 2
+                assert match.player_tables[0].team_status[0].usage == 1
+                assert match.player_tables[0].team_status[1].usage == 1
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
     test_woven_element()
+    test_enduring_rock()
