332b7c682deb379a3586a7fdc23986148792241e
zyr17 <jzjqz17@gmail.com>
2023-09-03 22:06:50 +0800

    test: heavy strike

--- tests/server/cards/test_event_cards.py
+++ tests/server/cards/test_event_cards.py
@@ -3,7 +3,8 @@ from agents.nothing_agent import NothingAgent
 from server.match import Match, MatchState
 from server.deck import Deck
 from tests.utils_for_test import (
-    get_random_state, get_test_id_from_command, make_respond, set_16_omni
+    check_hp, get_random_state, get_test_id_from_command, make_respond, 
+    set_16_omni
 )
 
 
@@ -674,10 +675,200 @@ def test_claxs_art():
     assert match.state != MatchState.ERROR
 
 
+def test_heavy_strike():
+    cmd_records = [
+        [
+            "sw_card",
+            "choose 0",
+            "card 0 0 0",
+            "sw_char 1 0",
+            "TEST 1 10 7 10 10 10 10",
+            "card 0 0 0",
+            "skill 0 0 1 2",
+            "TEST 1 10 4 10 7 10 10",
+            "TEST 2 p1c0 1 status",
+            "TEST 2 p0c0 1 status",
+            "end",
+            "TEST 2 p1c0 0 status",
+            "TEST 2 p0c0 0 status",
+            "end"
+        ],
+        [
+            "sw_card",
+            "choose 0",
+            "card 0 0 0",
+            "card 0 0 0",
+            "skill 0 0 1 2",
+            "TEST 1 10 7 10 7 10 10",
+            "card 0 0 0",
+            "skill 1 0 1 2",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:PyroMobMage
+        charactor:Arataki Itto
+        charactor:Noelle
+        Heavy Strike*30
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                # a sample of HP check based on the command string.
+                hps = cmd.strip().split(' ')[2:]
+                hps = [int(x) for x in hps]
+                hps = [hps[:3], hps[3:]]
+                check_hp(match, hps)
+            elif test_id == 2:
+                # test 2 p0c0 1 status
+                cmd = cmd.split()
+                pnum = int(cmd[2][1])
+                cnum = int(cmd[2][3])
+                count = int(cmd[3])
+                assert len(
+                    match.player_tables[pnum].charactors[cnum].status
+                ) == count
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
+def test_heavy_strike_2():
+    cmd_records = [
+        [
+            "sw_card",
+            "choose 0",
+            "skill 0 0 1 2",
+            "sw_char 1 0",
+            "card 0 0 0",
+            "skill 0 0 1 2",
+            "TEST 1 10 10 10 9 6 9",
+            "end"
+        ],
+        [
+            "sw_card",
+            "choose 1",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:ElectroMobMage
+        charactor:CryoMobMage
+        charactor:Noelle
+        Heavy Strike*30
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                # a sample of HP check based on the command string.
+                hps = cmd.strip().split(' ')[2:]
+                hps = [int(x) for x in hps]
+                hps = [hps[:3], hps[3:]]
+                check_hp(match, hps)
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
     # test_bestest()
     # test_changing_shifts()
     # test_toss_up()
     # test_old_i_havent_lost_yet()
     # test_leave_it_to_me()
-    test_claxs_art()
+    # test_claxs_art()
+    test_heavy_strike_2()
