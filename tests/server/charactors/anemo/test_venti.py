deafa1850b58238d592bc208df99176e9a4be0e9
zyr17 <jzjqz17@gmail.com>
2023-09-05 23:02:56 +0800

    test: add one test

--- tests/server/charactors/anemo/test_venti.py
+++ tests/server/charactors/anemo/test_venti.py
@@ -225,5 +225,109 @@ def test_venti():
     assert match.state != MatchState.ERROR
 
 
+def test_venti_2():
+    cmd_records = [
+        [
+            "sw_card",
+            "choose 1",
+            "sw_char 0 0",
+            "skill 1 0 1 2",
+            "skill 1 0 1 2",
+            "sw_char 1 0",
+            "card 0 0 0 1 2",
+            "end",
+            "sw_char 0",
+            "card 1 0",
+            "TEST 3 skill 0 cost 1",
+            "skill 0 0",
+            "TEST 2 p0 team usage 1 1",
+            "end"
+        ],
+        [
+            "sw_card",
+            "choose 0",
+            "sw_char 1 0",
+            "skill 1 0 1 2",
+            "end",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:Arataki Itto
+        charactor:Venti
+        charactor:Nahida
+        Northern Smoked Chicken*15
+        Embrace of Winds*15
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 2:
+                cmd = cmd.split()
+                pidx = int(cmd[2][1])
+                usage = [int(x) for x in cmd[5:]]
+                status = match.player_tables[pidx].team_status
+                assert len(status) == len(usage)
+                for s, u in zip(status, usage):
+                    assert s.usage == u
+            elif test_id == 3:
+                cmd = cmd.split()
+                sid = int(cmd[3])
+                c = int(cmd[5])
+                for req in match.requests:
+                    if req.name == 'UseSkillRequest':
+                        if req.skill_idx == sid:
+                            assert req.cost.total_dice_cost == c
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
-    test_venti()
+    test_venti_2()
