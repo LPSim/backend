5ae5a8841fee9970887ef6f141ba4e2a4ccff6a8
zyr17 <jzjqz17@gmail.com>
2023-09-05 22:57:41 +0800

    test: Northern Smoked Chicken

--- tests/server/cards/test_food.py
+++ tests/server/cards/test_food.py
@@ -886,6 +886,201 @@ def test_full_and_tandoori():
     assert match.state != MatchState.ERROR
 
 
+def test_north_chicken():
+    cmd_records = [
+        [
+            "sw_card",
+            "choose 0",
+            "TEST 1 card can use",
+            "TEST 2 skill 0 cost 3",
+            "card 0 0",
+            "TEST 2 skill 0 cost 2",
+            "TEST 2 skill 1 cost 3",
+            "sw_char 1 0",
+            "TEST 2 skill 0 cost 3",
+            "sw_char 0 0",
+            "skill 0 0 1",
+            "TEST 2 skill 0 cost 3",
+            "TEST 3 p0c0 status 1",
+            "end",
+            "card 0 0",
+            "end"
+        ],
+        [
+            "sw_card",
+            "choose 0",
+            "end",
+            "sw_char 1 0",
+            "sw_char 0 0",
+            "TEST 2 skill 0 cost 3",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:Arataki Itto
+        charactor:Mona
+        charactor:Nahida
+        Northern Smoked Chicken*30
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                # a sample of HP check based on the command string.
+                count = 0
+                for req in match.requests:
+                    if req.name == 'UseCardRequest':
+                        count += 1
+                assert count > 0
+            elif test_id == 2:
+                cmd = cmd.split()
+                sidx = int(cmd[3])
+                c = int(cmd[5])
+                for req in match.requests:
+                    if req.name == 'UseSkillRequest':
+                        if req.skill_idx == sidx:
+                            assert req.cost.total_dice_cost == c
+            elif test_id == 3:
+                assert len(match.player_tables[0].charactors[0].status) == 1
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
+def test_north_chicken_2():
+    cmd_records = [
+        [
+            "sw_card",
+            "choose 0",
+            "skill 1 0 1 2",
+            "skill 1 0 1 2",
+            "end",
+            "card 0 0 0 1",
+            "card 1 0",
+            "skill 0 1",
+            "sw_char 1 0",
+            "sw_char 2 0",
+            "sw_char 0 0",
+            "TEST 1 p0c0 status 3",
+            "skill 0 1",
+            "end"
+        ],
+        [
+            "sw_card",
+            "choose 0",
+            "skill 1 0 1 2",
+            "end",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:Arataki Itto
+        charactor:Mona
+        charactor:Nahida
+        Northern Smoked Chicken*15
+        Laurel Coronet*15
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                assert len(match.player_tables[0].charactors[0].status) == 3
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
     test_adeptus_temptation()
     test_lotus_flower_crisp()
@@ -894,3 +1089,4 @@ if __name__ == '__main__':
     test_tandoori()
     test_pizza()
     test_full_and_tandoori()
+    test_north_chicken()
