56b249c9a2701910fba3e48ca087ee9ba2997713
zyr17 <jzjqz17@gmail.com>
2023-09-06 10:15:58 +0800

    test: setaria

--- tests/server/supports/test_companions.py
+++ tests/server/supports/test_companions.py
@@ -471,7 +471,113 @@ def test_liben():
     assert match.state != MatchState.ERROR
 
 
+def test_setaria():
+    cmd_records = [
+        [
+            "sw_card",
+            "choose 0",
+            "reroll",
+            "card 2 0 0",
+            "card 3 0 0",
+            "tune 0 0",
+            "tune 0 0",
+            "card 0 0 0",
+            "TEST 1 p0 hand 2",
+            "card 0 0 0",
+            "tune 0 1",
+            "TEST 1 p0 hand 1",
+            "TEST 2 p0 support usage 2 3 3",
+            "tune 0 2",
+            "tune 0 3",
+            "card 0 0 0",
+            "TEST 2 p0 support usage 2 3 3",
+            "card 0 0 0",
+            "card 0 0 0",
+            "card 0 0 0",
+            "end",
+            "TEST 2 p0 support usage 2 3 3",
+            "reroll"
+        ],
+        [
+            "sw_card",
+            "choose 0",
+            "reroll",
+            "sw_char 1 0",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:Fischl
+        charactor:Mona
+        charactor:Nahida
+        Setaria*15
+        Strategize*15
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                cmd = cmd.split()
+                pid = int(cmd[2][1])
+                assert len(match.player_tables[pid].hands) == int(cmd[4])
+            elif test_id == 2:
+                cmd = cmd.split()
+                pid = int(cmd[2][1])
+                us = [int(x) for x in cmd[5:]]
+                assert len(match.player_tables[pid].supports) == len(us)
+                for support, u in zip(match.player_tables[pid].supports, us):
+                    assert support.usage == u
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
     test_rana()
     test_timmie()
     test_liben()
+    test_setaria()
