6a5e9b7f82c5b8bae2a172f3816365154358cb0d
zyr17 <jzjqz17@gmail.com>
2023-09-13 22:42:24 +0800

    feat: implement tighnari

--- server/charactor/dendro/__init__.py
+++ server/charactor/dendro/__init__.py
@@ -1,7 +1,10 @@
 from .nahida import Nahida, TheSeedOfStoredKnowledge
 from .collei import Collei, FloralSidewinder, CuileinAnbar
+from .tighnari import Tighnari, KeenSight, ClusterbloomArrow
 
 
-DendroCharactors = Nahida | Collei
-SummonsOfDendroCharactors = CuileinAnbar | CuileinAnbar
-DendroCharactorTalents = TheSeedOfStoredKnowledge | FloralSidewinder
+DendroCharactors = Nahida | Collei | Tighnari
+SummonsOfDendroCharactors = CuileinAnbar | ClusterbloomArrow
+DendroCharactorTalents = (
+    TheSeedOfStoredKnowledge | FloralSidewinder | KeenSight
+)
--- server/status/charactor_status/dendro_charactors.py
+++ server/status/charactor_status/dendro_charactors.py
@@ -2,18 +2,24 @@
 
 from typing import Any, List, Literal
 
-from ...struct import Cost
+from ...struct import Cost, ObjectPosition
 
-from ...modifiable_values import DamageValue
-
-from ...consts import (
-    DamageElementalType, DamageType, ElementType, ElementalReactionType
+from ...modifiable_values import (
+    CostValue, DamageElementEnhanceValue, DamageValue
 )
 
-from ...action import ChangeObjectUsageAction, MakeDamageAction
+from ...consts import (
+    CostLabels, DamageElementalType, DamageType, ElementType, 
+    ElementalReactionType, ObjectPositionType, SkillType
+)
 
-from ...event import ReceiveDamageEventArguments
-from .base import UsageCharactorStatus
+from ...action import (
+    ChangeObjectUsageAction, CreateObjectAction, MakeDamageAction, 
+    RemoveObjectAction
+)
+
+from ...event import ReceiveDamageEventArguments, SkillEndEventArguments
+from .base import ElementalInfusionCharactorStatus, UsageCharactorStatus
 
 
 class SeedOfSkandha(UsageCharactorStatus):
@@ -94,4 +100,94 @@ class SeedOfSkandha(UsageCharactorStatus):
         return actions
 
 
-DendroCharactorStatus = SeedOfSkandha | SeedOfSkandha
+class VijnanaSuffusion(ElementalInfusionCharactorStatus, UsageCharactorStatus):
+    name: Literal['Vijnana Suffusion'] = 'Vijnana Suffusion'
+    desc: str = (
+        'When the character to which this is attached to uses a Charged '
+        'Attack: Their Physical DMG dealt will be converted to Dendro DMG and '
+        'after skill DMG is finalized, summon 1 Clusterbloom Arrow.'
+    )
+    version: Literal['3.6'] = '3.6'
+    usage: int = 2
+    max_usage: int = 2
+    infused_elemental_type: DamageElementalType = DamageElementalType.DENDRO
+
+    def value_modifier_DAMAGE_ELEMENT_ENHANCE(
+        self, value: DamageElementEnhanceValue, match: Any, 
+        mode: Literal['TEST', 'REAL']
+    ) -> DamageElementEnhanceValue:
+        """
+        When character use charged attack, change damage type to dendro
+        """
+        if not value.is_corresponding_charactor_use_damage_skill(
+            self.position, match, SkillType.NORMAL_ATTACK
+        ):
+            # not use normal attack
+            return value
+        if not match.player_tables[self.position.player_idx].charge_satisfied:
+            # not charged attack, not modify
+            return value
+        return super().value_modifier_DAMAGE_ELEMENT_ENHANCE(
+            value, match, mode)
+
+    def value_modifier_COST(
+        self, value: CostValue, match: Any, mode: Literal['TEST', 'REAL']
+    ) -> CostValue:
+        """
+        If talent equipped, and is charged attack, decrease 1 any cost
+        """
+        charactor = match.player_tables[self.position.player_idx].charactors[
+            self.position.charactor_idx]
+        if charactor.talent is None:
+            # no talent, not modify
+            return value
+        if not self.position.check_position_valid(
+            value.position, match, player_idx_same = True,
+            charactor_idx_same = True, target_area = ObjectPositionType.SKILL,
+        ):
+            # not this charactor use skill, not modify
+            return value
+        if not (
+            match.player_tables[self.position.player_idx].charge_satisfied
+            and value.cost.label & CostLabels.NORMAL_ATTACK.value != 0
+        ):
+            # not charged attack, not modify
+            return value
+        # decrease
+        value.cost.decrease_cost(None)
+        return value
+
+    def event_handler_SKILL_END(
+        self, event: SkillEndEventArguments, match: Any
+    ) -> List[CreateObjectAction | RemoveObjectAction]:
+        """
+        If self use charged attack, summon Clusterbloom Arrow
+        """
+        if not self.position.check_position_valid(
+            event.action.position, match, player_idx_same = True,
+            charactor_idx_same = True, target_area = ObjectPositionType.SKILL,
+        ):
+            # not same player or charactor, or not skill, not modify
+            return []
+        if not (
+            match.player_tables[self.position.player_idx].charge_satisfied
+            and match.get_object(event.action.position).skill_type
+            == SkillType.NORMAL_ATTACK
+        ):
+            # not charged attack, not modify
+            return []
+        assert self.usage > 0
+        # summon
+        self.usage -= 1
+        return [CreateObjectAction(
+            object_position = ObjectPosition(
+                player_idx = self.position.player_idx,
+                area = ObjectPositionType.SUMMON,
+                id = 0
+            ),
+            object_name = 'Clusterbloom Arrow',
+            object_arguments = {}
+        )] + self.check_should_remove()
+
+
+DendroCharactorStatus = SeedOfSkandha | VijnanaSuffusion
