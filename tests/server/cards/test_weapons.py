304bf85275fff8f0e8eb08cbc7830611eabd0d03
zyr17 <jzjqz17@gmail.com>
2023-09-15 12:27:40 +0800

    test: lithic spear

--- tests/server/cards/test_weapons.py
+++ tests/server/cards/test_weapons.py
@@ -597,8 +597,153 @@ def test_vortex_2():
     assert match.state != MatchState.ERROR
 
 
+def test_lithic_spear():
+    cmd_records = [
+        [
+            "sw_card 1 3",
+            "choose 1",
+            "card 1 0 15 14 13",
+            "TEST 2 p0c1 usage 3",
+            "skill 1 12 11 10",
+            "skill 0 9 8 7",
+            "skill 0 6 5 4",
+            "skill 1 3 2 1",
+            "end",
+            "card 1 0 15 14 13",
+            "skill 0 12 11 10",
+            "skill 0 9 8 7",
+            "card 0 0",
+            "card 4 0 6",
+            "TEST 2 p0c1 usage 3",
+            "skill 0 5 4 3",
+            "skill 1 2 1 0",
+            "end",
+            "skill 1 15 14 13",
+            "choose 4",
+            "TEST 1 10 0 10 10 10 10 10 0 0 3",
+            "end",
+            "card 3 1 15 14 13",
+            "skill 1 12 11 10",
+            "skill 0 9 8 7",
+            "skill 2 6 5 4",
+            "skill 1 3 2 1",
+            "end"
+        ],
+        [
+            "sw_card 1 2 4",
+            "choose 3",
+            "TEST 2 p0c1 usage 3 2",
+            "skill 1 15 14 13",
+            "TEST 2 p0c1 usage",
+            "TEST 1 10 10 10 10 10 10 10 10 6 10",
+            "end",
+            "choose 2",
+            "skill 1 15 14",
+            "sw_char 4 13",
+            "skill 1 12 11 10",
+            "sw_char 2 9",
+            "skill 0 8 7 6",
+            "choose 4",
+            "card 0 1 5 4 3",
+            "TEST 2 p1c4 usage 3",
+            "skill 0 2 1 0",
+            "end",
+            "skill 2 15 14 13",
+            "TEST 1 10 5 10 10 10 10 10 0 0 3",
+            "card 0 1 12 11 10",
+            "TEST 2 p1c4 usage 2",
+            "skill 1 9 8 7",
+            "end",
+            "end",
+            "choose 0",
+            "choose 1",
+            "card 4 0 15 14 13",
+            "TEST 2 p1c1 usage",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:Xingqiu
+        charactor:Candace
+        charactor:Hu Tao
+        charactor:Chongyun
+        charactor:Shenhe
+        Lithic Spear*10
+        Lithic Spear@3.3*10
+        Where Is the Unseen Razor?*10
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                # a sample of HP check based on the command string.
+                hps = cmd.strip().split(' ')[2:]
+                hps = [int(x) for x in hps]
+                hps = [hps[:5], hps[5:]]
+                check_hp(match, hps)
+            elif test_id == 2:
+                cmd = cmd.split()
+                pidx = int(cmd[2][1])
+                cidx = int(cmd[2][3])
+                usages = [int(x) for x in cmd[4:]]
+                status = match.player_tables[pidx].charactors[cidx].status
+                assert len(usages) == len(status)
+                for u, s in zip(usages, status):
+                    assert u == s.usage
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
     test_vanilla_weapons()
     test_the_bell()
     test_vortex_vanquisher()
     test_vortex_2()
+    test_lithic_spear()
