4ea3cc402881dcb8443fefef5ea49fde6d37900e
zyr17 <jzjqz17@gmail.com>
2023-09-15 23:47:41 +0800

    test: Fruit of Fulfillment

--- tests/server/cards/test_weapons.py
+++ tests/server/cards/test_weapons.py
@@ -1033,6 +1033,99 @@ def test_amos():
     assert match.state != MatchState.ERROR
 
 
+def test_fruit_of_fullfillment():
+    cmd_records = [
+        [
+            "sw_card",
+            "choose 1",
+            "card 2 2 15 14 13",
+            "card 0 2 12 11 10",
+            "card 2 0",
+            "card 5 2 9",
+            "TEST 1 p0 hand 8",
+            "card 4 0",
+            "card 4 2 8",
+            "card 5 0",
+            "card 4 2 7",
+            "TEST 1 p0 hand 10",
+            "card 4 2 6 5 4",
+            "TEST 1 p0 hand 10",
+            "end"
+        ],
+        [
+            "sw_card",
+            "choose 0",
+            "card 0 0 15 14 13",
+            "TEST 1 p1 hand 6",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:Nahida
+        charactor:Mona
+        charactor:Klee
+        Where Is the Unseen Razor?*10
+        Fruit of Fulfillment*10
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                cmd = cmd.split()
+                pidx = int(cmd[2][1])
+                hand = match.player_tables[pidx].hands
+                assert len(hand) == int(cmd[4])
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
     # test_vanilla_weapons()
     # test_the_bell()
@@ -1041,4 +1134,5 @@ if __name__ == '__main__':
     # test_lithic_spear()
     # test_kings_squire()
     # test_kings_squire_2()
-    test_amos()
+    # test_amos()
+    test_fruit_of_fullfillment()
