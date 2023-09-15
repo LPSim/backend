f1dc2a682d46bc491cc5c8409fad017987595ee7
zyr17 <jzjqz17@gmail.com>
2023-09-16 00:58:50 +0800

    test: Wolf's Gravestone

--- tests/server/cards/test_weapons.py
+++ tests/server/cards/test_weapons.py
@@ -1237,6 +1237,97 @@ def test_sacrificial():
     assert match.state != MatchState.ERROR
 
 
+def test_wolf_gravestone():
+    cmd_records = [
+        [
+            "sw_card",
+            "choose 0",
+            "card 0 0 15 14 13",
+            "skill 1 12 11 10",
+            "skill 1 9 8 7",
+            "sw_char 1 6",
+            "end"
+        ],
+        [
+            "sw_card",
+            "choose 1",
+            "skill 1 15 14 13",
+            "choose 0",
+            "card 0 0 12 11 10",
+            "skill 1 9 8 7",
+            "skill 0 6 5 4",
+            "skill 0 3 2 1",
+            "TEST 1 4 1 9 10 0 10",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:Chongyun
+        charactor:Fischl
+        charactor:Yae Miko
+        Where Is the Unseen Razor?*10
+        Wolf's Gravestone*10
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                # a sample of HP check based on the command string.
+                hps = cmd.strip().split(' ')[2:]
+                hps = [int(x) for x in hps]
+                hps = [hps[:3], hps[3:]]
+                check_hp(match, hps)
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
     # test_vanilla_weapons()
     # test_the_bell()
@@ -1247,4 +1338,5 @@ if __name__ == '__main__':
     # test_kings_squire_2()
     # test_amos()
     # test_fruit_of_fullfillment()
-    test_sacrificial()
+    # test_sacrificial()
+    test_wolf_gravestone()
