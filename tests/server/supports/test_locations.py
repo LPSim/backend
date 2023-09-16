7633802a9231ae80cef9cd95eb90bf54eb2d48e5
zyr17 <jzjqz17@gmail.com>
2023-09-17 01:20:04 +0800

    test: inn, cathedral, sangonomiya shrine

--- tests/server/supports/test_locations.py
+++ tests/server/supports/test_locations.py
@@ -2,7 +2,7 @@ from agents.interaction_agent import InteractionAgent
 from server.match import Match, MatchState
 from server.deck import Deck
 from tests.utils_for_test import (
-    get_test_id_from_command, make_respond, 
+    check_hp, get_test_id_from_command, make_respond, 
     get_random_state, set_16_omni
 )
 
@@ -648,9 +648,160 @@ def test_library():
     assert match.state != MatchState.ERROR
 
 
+def test_cathedral_inn_sangonomiya():
+    cmd_records = [
+        [
+            "sw_card 1 3 2",
+            "choose 0",
+            "skill 1 15 14 13",
+            "card 0 0 12 11",
+            "card 0 0 10 9",
+            "card 0 0 8 7",
+            "end",
+            "TEST 1 9 10 10 10 10 10 10 10",
+            "TEST 2 p0 support 2 2 2",
+            "TEST 2 p1 support 2 1",
+            "sw_char 1 15",
+            "skill 0 14 13 12",
+            "skill 1 11 10 9",
+            "end",
+            "sw_char 3 15",
+            "skill 2 14 13 12 11 10",
+            "skill 1 9 8 7",
+            "sw_char 1 6",
+            "skill 2 5 4 3 2",
+            "end",
+            "TEST 1 10 10 10 10 5 10 0 9",
+            "TEST 2 p0 support 1 1",
+            "skill 0 15 14 13",
+            "skill 1 12 11 10",
+            "end",
+            "TEST 1 10 10 10 10 0 10 0 9",
+            "sw_char 3 15",
+            "skill 2 14 13 12 11 10",
+            "skill 3 9 8 7",
+            "end",
+            "TEST 1 10 10 10 5 0 0 0 7",
+            "TEST 2 p1 support 2",
+            "TEST 2 p0 support 1",
+            "end"
+        ],
+        [
+            "sw_card 1 3 2",
+            "choose 0",
+            "skill 1 15 14 13",
+            "card 1 0 12 11",
+            "card 1 0 10 9",
+            "end",
+            "sw_char 2 15",
+            "skill 0 14 13 12",
+            "end",
+            "TEST 2 p0 support 1 1 2",
+            "TEST 2 p1 support 1 1",
+            "TEST 1 10 10 10 10 10 10 8 10",
+            "sw_char 3 15",
+            "sw_char 2 14",
+            "skill 0 13 12 11",
+            "sw_char 3 10",
+            "sw_char 2 9",
+            "choose 3",
+            "sw_char 0 8",
+            "end",
+            "sw_char 1 15",
+            "skill 0 14 13 12",
+            "sw_char 0 11",
+            "card 0 0 10 9",
+            "end",
+            "choose 1",
+            "card 2 0 15 14",
+            "skill 0 13 12 11",
+            "skill 0 10 9 8",
+            "skill 0 7 6 5",
+            "end",
+            "choose 3"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:Mona
+        charactor:Xiangling
+        charactor:Fischl
+        charactor:Ganyu
+        Favonius Cathedral*10
+        Wangshu Inn*10
+        Sangonomiya Shrine*10
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                # a sample of HP check based on the command string.
+                hps = cmd.strip().split(' ')[2:]
+                hps = [int(x) for x in hps]
+                hps = [hps[:4], hps[4:]]
+                check_hp(match, hps)
+            elif test_id == 2:
+                cmd = cmd.split()
+                pidx = int(cmd[2][1])
+                usages = [int(x) for x in cmd[4:]]
+                supports = match.player_tables[pidx].supports
+                assert len(supports) == len(usages)
+                for u, s in zip(usages, supports):
+                    assert u == s.usage
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
     # test_liyue_harbor()
     # test_tenshukaku()
     # test_sumeru_city()
     # test_vanarana()
-    test_library()
+    # test_library()
+    test_cathedral_inn_sangonomiya()
