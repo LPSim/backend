68859cfed5e002c47b6310628f504fce777a9984
zyr17 <jzjqz17@gmail.com>
2023-09-16 17:52:05 +0800

    test: NRE

--- tests/server/supports/test_items.py
+++ tests/server/supports/test_items.py
@@ -8,16 +8,6 @@ from tests.utils_for_test import (
 
 
 def test_seelie():
-    # use frontend and FastAPI server to perform commands, and test commands 
-    # that start with TEST. NO NOT put TEST at the end of command list!
-    # If a command is succesfully performed, frontend will print history 
-    # commands in console. Note that frontend cannot distinguish if a new
-    # match begins, so you need to refresh the page before recording a new
-    # match, otherwise the history commands will be mixed.
-    #
-    # for tests, it starts with TEST and contains a test id, which is used to
-    # identify the test. Other texts in the command are ignored, but you can
-    # parse them if you want. Refer to the following code.
     cmd_records = [
         [
             "sw_card",
@@ -118,5 +108,126 @@ def test_seelie():
     assert match.state != MatchState.ERROR
 
 
+def test_NRE():
+    cmd_records = [
+        [
+            "sw_card 1",
+            "choose 0",
+            "skill 1 15 14 13 12 11",
+            "card 0 0 10 9",
+            "card 3 0",
+            "sw_char 1 8",
+            "end",
+            "card 4 0",
+            "TEST 1 p0 support usage 0",
+            "TEST 2 p0 card 7",
+            "end"
+        ],
+        [
+            "sw_card 1 3",
+            "choose 0",
+            "sw_char 1 15",
+            "sw_char 2 14",
+            "card 1 2",
+            "TEST 2 p1 hand 4",
+            "card 0 0 13 12",
+            "TEST 2 p1 card 4",
+            "card 0 0 11 10",
+            "TEST 2 p1 card 4",
+            "card 0 0 9 8",
+            "TEST 2 p1 card 4",
+            "TEST 1 p1 support usage 0 0",
+            "skill 1 7 6 5",
+            "TEST 2 p0 card 5",
+            "end",
+            "card 3 0 15 14",
+            "TEST 2 p0 card 7",
+            "TEST 2 p1 card 6",
+            "TEST 1 p1 support usage 0 0",
+            "TEST 1 p0 support usage 1",
+            "end",
+            "card 5 0 15 14",
+            "card 4 0 13 12",
+            "TEST 2 p1 card 6",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:Electro Hypostasis
+        charactor:Klee
+        charactor:Keqing
+        NRE*15
+        Sweet Madame*2
+        Mushroom Pizza*2
+        Tandoori Roast Chicken*2
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                cmd = cmd.split()
+                pidx = int(cmd[2][1])
+                usage = [int(x) for x in cmd[5:]]
+                supports = match.player_tables[pidx].supports
+                assert len(usage) == len(supports)
+                for u, s in zip(usage, supports):
+                    assert u == s.usage
+            elif test_id == 2:
+                cmd = cmd.split()
+                pidx = int(cmd[2][1])
+                cardnum = int(cmd[4])
+                assert len(match.player_tables[pidx].hands) == cardnum
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
     test_seelie()
+    test_NRE()
