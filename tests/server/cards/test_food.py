66f4445729cf0368590273a44a8cab17f5eaab95
zyr17 <jzjqz17@gmail.com>
2023-09-19 09:56:08 +0800

    test: guoba meatroll egg sashimi club

--- tests/server/cards/test_food.py
+++ tests/server/cards/test_food.py
@@ -1090,12 +1090,395 @@ def test_north_chicken_2():
     assert match.state != MatchState.ERROR
 
 
+def test_guoba_meatroll_egg_sashimi_crab():
+    cmd_records = [
+        [
+            "sw_card 1 2",
+            "choose 0",
+            "card 0 0 15",
+            "skill 2 14 13 12 11 10",
+            "TEST 1 9 10 10 8 7 8",
+            "sw_char 1 9",
+            "TEST 1 9 7 10 8 7 8",
+            "TEST 2 p1c1 usage 1",
+            "skill 1 8 7 6",
+            "sw_char 0 5",
+            "end",
+            "card 4 0 15",
+            "skill 2 14 13 12 11 10",
+            "TEST 1 8 7 10 4 0 4",
+            "skill 0 9 8 7",
+            "TEST 1 8 7 10 1 0 4",
+            "TEST 2 p0c0 usage 1 1",
+            "TEST 3 card 2 cannot use",
+            "end",
+            "card 0 0 15",
+            "sw_char 1 14",
+            "TEST 1 5 6 10 0 0 4",
+            "sw_char 2 13",
+            "sw_char 1 12",
+            "TEST 1 2 2 7 0 0 4",
+            "sw_char 0 11",
+            "sw_char 1 10",
+            "choose 2",
+            "TEST 1 2 0 7 0 0 4",
+            "TEST 3 card 0 cannot use",
+            "card 1 0 9 8 7",
+            "TEST 4 no card can use",
+            "end",
+            "sw_char 1 15",
+            "choose 0",
+            "end",
+            "choose 2",
+            "card 1 0 15 14 13",
+            "TEST 3 card 6 cannot use",
+            "sw_char 0 12",
+            "choose 2",
+            "end"
+        ],
+        [
+            "sw_card 4 2",
+            "choose 1",
+            "TEST 1 10 10 10 8 7 8",
+            "card 1 1",
+            "skill 1 15 14 13",
+            "skill 0 12 11 10",
+            "end",
+            "end",
+            "choose 0",
+            "choose 2",
+            "card 5 0 15",
+            "skill 0 14 13",
+            "skill 1 12 11 10",
+            "skill 0 9 8",
+            "skill 2 7 6 5 4",
+            "skill 0 3 2",
+            "skill 0 1 0",
+            "end",
+            "card 5 0",
+            "skill 1 15 14 13",
+            "skill 0 12 11 10",
+            "end",
+            "card 6 0 15",
+            "skill 0 14 13",
+            "skill 0 12 11",
+            "skill 0 10 9",
+            "TEST 5 skill 0 cost 3",
+            "TEST 2 p1c2 usage 1 1",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:Ganyu
+        charactor:Fischl
+        charactor:Keqing
+        Jueyun Guoba*5
+        Minty Meat Rolls*5
+        Minty Meat Rolls@3.3*5
+        Teyvat Fried Egg*5
+        Sashimi Platter*5
+        Butter Crab*5
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    match.config.check_deck_restriction = False
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                # a sample of HP check based on the command string.
+                hps = cmd.strip().split(' ')[2:]
+                hps = [int(x) for x in hps]
+                hps = [hps[:3], hps[3:]]
+                check_hp(match, hps)
+            elif test_id == 2:
+                cmd = cmd.split()
+                pidx = int(cmd[2][1])
+                cidx = int(cmd[2][3])
+                usage = [int(x) for x in cmd[4:]]
+                status = match.player_tables[pidx].charactors[cidx].status
+                assert len(status) == len(usage)
+                for s, u in zip(status, usage):
+                    assert s.usage == u
+            elif test_id == 3:
+                cmd = cmd.split()
+                for req in match.requests:
+                    if req.name == 'UseCardRequest':
+                        assert req.card_idx != int(cmd[3])
+            elif test_id == 4:
+                for req in match.requests:
+                    assert req.name != 'UseCardRequest'
+            elif test_id == 5:
+                for req in match.requests:
+                    if req.name == 'UseSkillRequest' and req.skill_idx == 0:
+                        assert req.cost.total_dice_cost == 3
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
+def test_meatroll():
+    cmd_records = [
+        [
+            "sw_card",
+            "choose 0",
+            "skill 1 0 1 2",
+            "skill 1 0 1 2",
+            "end",
+            "card 0 0 0 1",
+            "card 1 0 1",
+            "card 2 0 1",
+            "skill 0 1",
+            "sw_char 1 0",
+            "sw_char 2 0",
+            "sw_char 0 0",
+            "TEST 1 p0c0 status 3",
+            "skill 0 1",
+            "end"
+        ],
+        [
+            "sw_card",
+            "choose 0",
+            "skill 1 0 1 2",
+            "end",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:Arataki Itto
+        charactor:Mona
+        charactor:Nahida
+        Minty Meat Rolls*15
+        Laurel Coronet*15
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    match.config.check_deck_restriction = False
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                assert len(match.player_tables[0].charactors[0].status) == 3
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
+def test_guoba_egg():
+    cmd_records = [
+        [
+            "sw_card 1 2",
+            "choose 0",
+            "card 0 0 15",
+            "skill 1 14 13 12",
+            "TEST 1 8 8 8 9 10 10",
+            "TEST 2 card 2 cannot use",
+            "sw_char 2 11",
+            "sw_char 0 10",
+            "skill 0 9 8 7",
+            "skill 0 6 5 4",
+            "skill 1 3 2 1",
+            "end",
+            "choose 2",
+            "card 2 0 15 14 13",
+            "sw_char 0 12",
+            "skill 1 11 10 9",
+            "skill 0 8 7 6",
+            "choose 1",
+            "TEST 2 card 2 cannot use",
+            "TEST 1 0 7 7 9 9 1",
+            "end"
+        ],
+        [
+            "sw_card 2 3 4 0",
+            "choose 0",
+            "TEST 1 10 10 10 9 10 10",
+            "card 0 0",
+            "skill 2 15 14 13 12 11",
+            "sw_char 2 10",
+            "skill 1 9 8 7",
+            "skill 1 6 5 4",
+            "end",
+            "skill 1 15 14 13",
+            "sw_char 1 12",
+            "sw_char 2 11",
+            "skill 0 10 9 8"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        charactor:Ganyu
+        charactor:Fischl
+        charactor:Keqing
+        Jueyun Guoba*5
+        Minty Meat Rolls*5
+        Minty Meat Rolls@3.3*5
+        Teyvat Fried Egg*5
+        Sashimi Platter*5
+        Butter Crab*5
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    match.config.check_deck_restriction = False
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                # a sample of HP check based on the command string.
+                hps = cmd.strip().split(' ')[2:]
+                hps = [int(x) for x in hps]
+                hps = [hps[:3], hps[3:]]
+                check_hp(match, hps)
+            elif test_id == 2:
+                cmd = cmd.split()
+                for req in match.requests:
+                    if req.name == 'UseCardRequest':
+                        assert req.card_idx != int(cmd[3])
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
-    test_adeptus_temptation()
-    test_lotus_flower_crisp()
-    test_lotus_flower_crisp_and_reflection()
-    test_mond_hash()
-    test_tandoori()
-    test_pizza()
-    test_full_and_tandoori()
-    test_north_chicken()
+    # test_adeptus_temptation()
+    # test_lotus_flower_crisp()
+    # test_lotus_flower_crisp_and_reflection()
+    # test_mond_hash()
+    # test_tandoori()
+    # test_pizza()
+    # test_full_and_tandoori()
+    # test_north_chicken()
+    # test_north_chicken_2()
+    test_guoba_meatroll_egg_sashimi_crab()
+    test_meatroll()
+    test_guoba_egg()
