2257e481c1420b4a25708d80cf983ac7d5a999c1
zyr17 <jzjqz17@gmail.com>
2023-09-22 01:48:00 +0800

    test: red feather fan

--- tests/server/supports/test_items.py
+++ tests/server/supports/test_items.py
@@ -2,7 +2,7 @@ from src.lpsim.agents.interaction_agent import InteractionAgent
 from src.lpsim.server.match import Match, MatchState
 from src.lpsim.server.deck import Deck
 from tests.utils_for_test import (
-    get_random_state, get_test_id_from_command, make_respond, 
+    check_usage, get_random_state, get_test_id_from_command, make_respond, 
     set_16_omni
 )
 
@@ -356,7 +356,140 @@ def test_parametric():
     assert match.state != MatchState.ERROR
 
 
+def test_red_feather_fan():
+    cmd_records = [
+        [
+            "sw_card 1 3",
+            "choose 0",
+            "sw_char 1 15",
+            "TEST 1 p1 support 1 1",
+            "card 1 0 14 13",
+            "card 0 0",
+            "card 1 0",
+            "sw_char 2",
+            "sw_char 1",
+            "sw_char 0 12",
+            "TEST 1 p1 support 0 0",
+            "TEST 2 p1 dice 10",
+            "end",
+            "sw_char 1 15",
+            "skill 1 14 13 12",
+            "TEST 1 p1 support 0 0",
+            "TEST 2 p1 dice 14",
+            "card 1 0",
+            "end",
+            "sw_char 2",
+            "card 1 0",
+            "card 2 0",
+            "sw_char 1",
+            "TEST 1 p0 support 1",
+            "sw_char 0"
+        ],
+        [
+            "sw_card 2 3",
+            "choose 0",
+            "card 0 0 15 14",
+            "card 0 0 13 12",
+            "TEST 3 sw cost 1",
+            "sw_char 1 11",
+            "TEST 2 p0 dice 12",
+            "TEST 1 p0 support 0",
+            "sw_char 0",
+            "card 0 0",
+            "sw_char 2",
+            "sw_char 1 10",
+            "end",
+            "sw_char 2 15",
+            "card 1 0",
+            "sw_char 1",
+            "TEST 1 p1 support 0 1",
+            "card 1 0",
+            "sw_char 0",
+            "sw_char 1 14",
+            "end",
+            "sw_char 2 15"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        default_version:4.0
+        charactor:Diona
+        charactor:Noelle
+        charactor:Sucrose
+        Red Feather Fan*10
+        Changing Shifts*10
+        Leave It to Me!*10
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    match.config.check_deck_restriction = False
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                # a sample of HP check based on the command string.
+                cmd = cmd.split()
+                pidx = int(cmd[2][1])
+                check_usage(match.player_tables[pidx].supports, cmd[4:])
+            elif test_id == 2:
+                cmd = cmd.split()
+                pidx = int(cmd[2][1])
+                assert len(
+                    match.player_tables[pidx].dice.colors) == int(cmd[-1])
+            elif test_id == 3:
+                for req in match.requests:
+                    if req.name == 'SwitchCharactorRequest':
+                        assert req.cost.total_dice_cost == 1
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
     test_seelie()
     test_NRE()
     test_parametric()
+    test_red_feather_fan()
