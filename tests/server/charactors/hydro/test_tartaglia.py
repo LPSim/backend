28a125a5096e5399de29461aed024d7b92c973ee
zyr17 <jzjqz17@gmail.com>
2023-10-01 17:27:15 +0800

    test: tartaglia ver 4.1

--- tests/server/charactors/hydro/test_tartaglia.py
+++ tests/server/charactors/hydro/test_tartaglia.py
@@ -290,6 +290,136 @@ def test_tartaglia_2():
     assert match.state != MatchState.ERROR
 
 
+def test_tartaglia_4_1():
+    cmd_records = [
+        [
+            "sw_card",
+            "choose 0",
+            "skill 1 15 14 13",
+            "TEST 2 p0c0 usage 2",
+            "sw_char 2 12",
+            "sw_char 3 11",
+            "sw_char 2 10",
+            "sw_char 3 9",
+            "sw_char 2 8",
+            "TEST 1 8 10 8 8 10 10 8 10 10 10",
+            "TEST 2 p0c3 usage 2",
+            "end",
+            "TEST 1 8 10 8 8 10 10 8 10 10 10",
+            "TEST 2 p1c1 usage 1 1",
+            "TEST 2 p0c3 usage 1",
+            "sw_char 0 15",
+            "skill 0 14 13 12",
+            "skill 0 11 10 9",
+            "sw_char 3 8",
+            "end",
+            "sw_char 0 15",
+            "skill 2 14 13 12",
+            "sw_char 4 11",
+            "TEST 1 8 10 8 7 6 10 8 5 8 8",
+            "TEST 2 p0c4 usage 2",
+            "TEST 2 p1c2 usage 1",
+            "end"
+        ],
+        [
+            "sw_card",
+            "choose 1",
+            "TEST 2 p1c1 usage 1 1",
+            "skill 1 15 14 13",
+            "sw_char 3 12",
+            "sw_char 2 11",
+            "sw_char 1 10",
+            "skill 0 9 8 7",
+            "card 0 0 6 5 4",
+            "end",
+            "sw_char 3 15",
+            "sw_char 4 14",
+            "TEST 2 p1c4 usage 1",
+            "end",
+            "TEST 1 8 10 8 7 10 10 8 10 8 8",
+            "sw_char 3 15",
+            "sw_char 2 14",
+            "sw_char 1 13",
+            "skill 2 12 11 10"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        default_version:4.1
+        charactor:Tartaglia
+        charactor:Tartaglia@3.7
+        charactor:Venti
+        charactor:ElectroMobMage
+        charactor:CryoMobMage
+        Abyssal Mayhem: Hydrospout*15
+        Abyssal Mayhem: Hydrospout@3.7*15
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    match.config.check_deck_restriction = False
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                # a sample of HP check based on the command string.
+                hps = cmd.strip().split(' ')[2:]
+                hps = [int(x) for x in hps]
+                hps = [hps[:5], hps[5:]]
+                check_hp(match, hps)
+            elif test_id == 2:
+                cmd = cmd.split()
+                pidx, cidx = get_pidx_cidx(cmd)
+                status = match.player_tables[pidx].charactors[cidx].status
+                check_usage(status, cmd[4:])
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
     test_tartaglia()
     test_tartaglia_2()
+    test_tartaglia_4_1()
