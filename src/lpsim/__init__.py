8621b793bdcc452d9b1950c1794399632e6fe9a0
zyr17 <jzjqz17@gmail.com>
2023-10-29 00:07:20 +0800

    chore: catch error when no version file

--- src/lpsim/__init__.py
+++ src/lpsim/__init__.py
@@ -1,3 +1,9 @@
 from .server.deck import Deck  # noqa: F401
 from .server.match import Match, MatchState  # noqa: F401
-from ._version import __version__, __version_tuple__  # noqa: F401
+
+
+try:
+    from ._version import __version__, __version_tuple__  # noqa: F401
+except ModuleNotFoundError:
+    __version__ = 'unknown'
+    __version_tuple__ = (0, 0, 0)
