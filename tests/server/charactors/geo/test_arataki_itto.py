de1db788fce9b970ad76f15db6e9e224b94e058b
zyr17 <jzjqz17@gmail.com>
2023-11-03 15:38:37 +0800

    test: arataki itto

--- tests/server/charactors/geo/test_arataki_itto.py
+++ tests/server/charactors/geo/test_arataki_itto.py
@@ -2,7 +2,7 @@ from src.lpsim.agents.interaction_agent import InteractionAgent
 from src.lpsim.server.match import Match, MatchState
 from src.lpsim.server.deck import Deck
 from tests.utils_for_test import (
-    check_hp, get_random_state, get_test_id_from_command, make_respond, 
+    check_hp, check_usage, get_pidx_cidx, get_random_state, get_test_id_from_command, make_respond, 
     set_16_omni
 )
 
@@ -296,6 +296,102 @@ def test_arataki_itto_2():
     assert match.state != MatchState.ERROR
 
 
+def test_arataki_itto_3():
+    cmd_records = [
+        [
+            "sw_card",
+            "choose 1",
+            "skill 0 15 14 13",
+            "skill 1 12 11 10",
+            "card 1 0 9",
+            "skill 1 8 7 6",
+            "end",
+            "TEST 1 p1c1 usage 1 2",
+            "TEST 1 p0c1 usage 3 1",
+            "end"
+        ],
+        [
+            "sw_card",
+            "choose 1",
+            "skill 0 15 14 13",
+            "sw_char 2 12",
+            "sw_char 1 11",
+            "card 1 0 10",
+            "TEST 1 p0c1 usage 2 1 2",
+            "skill 1 9 8 7",
+            "TEST 1 p0c1 usage 3 1 2",
+            "TEST 1 p1c1 usage 1 2 1",
+            "end"
+        ]
+    ]
+    agent_0 = InteractionAgent(
+        player_idx = 0,
+        verbose_level = 0,
+        commands = cmd_records[0],
+        only_use_command = True
+    )
+    agent_1 = InteractionAgent(
+        player_idx = 1,
+        verbose_level = 0,
+        commands = cmd_records[1],
+        only_use_command = True
+    )
+    # initialize match. It is recommended to use default random state to make
+    # replay unchanged.
+    match = Match(random_state = get_random_state())
+    # deck information
+    deck = Deck.from_str(
+        '''
+        default_version:4.1
+        charactor:Beidou
+        charactor:Arataki Itto
+        charactor:Klee
+        Mushroom Pizza*30
+        '''
+    )
+    match.set_deck([deck, deck])
+    match.config.max_same_card_number = None
+    match.config.charactor_number = None
+    match.config.card_number = None
+    match.config.check_deck_restriction = False
+    # check whether random_first_player is enabled.
+    match.config.random_first_player = False
+    # check whether in rich mode (16 omni each round)
+    set_16_omni(match)
+    match.start()
+    match.step()
+
+    while True:
+        if match.need_respond(0):
+            agent = agent_0
+        elif match.need_respond(1):
+            agent = agent_1
+        else:
+            raise AssertionError('No need respond.')
+        # do tests
+        while True:
+            cmd = agent.commands[0]
+            test_id = get_test_id_from_command(agent)
+            if test_id == 0:
+                # id 0 means current command is not a test command.
+                break
+            elif test_id == 1:
+                cmd = cmd.strip().split(' ')
+                pidx, cidx = get_pidx_cidx(cmd)
+                check_usage(match.player_tables[pidx].charactors[cidx].status,
+                            cmd[4:])
+            else:
+                raise AssertionError(f'Unknown test id {test_id}')
+        # respond
+        make_respond(agent, match)
+        if len(agent_1.commands) == 0 and len(agent_0.commands) == 0:
+            break
+
+    # simulate ends, check final state
+    assert match.state != MatchState.ERROR
+
+
 if __name__ == '__main__':
-    test_arataki_itto()
-    test_arataki_itto_2()
+    # test_arataki_itto()
+    # test_arataki_itto_2()
+    test_arataki_itto_3()
