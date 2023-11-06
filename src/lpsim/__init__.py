54df26eee6ff8d2adf03e19a8b560aa6bdb9a74a
zyr17 <jzjqz17@gmail.com>
2023-11-07 00:35:21 +0800

    chore: fix lint error

--- src/lpsim/__init__.py
+++ src/lpsim/__init__.py
@@ -3,7 +3,7 @@ from .server.match import Match, MatchState  # noqa: F401
 
 
 try:
-    from ._version import __version__, __version_tuple__  # noqa: F401
+    from ._version import __version__, __version_tuple__  # type: ignore
 except ModuleNotFoundError:
     __version__ = 'unknown'
     __version_tuple__ = (0, 0, 0)
