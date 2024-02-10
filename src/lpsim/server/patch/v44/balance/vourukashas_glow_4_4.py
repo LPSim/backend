53608f0d3df80d995ea47c2b66f0982422593694
zyr17 <jzjqz17@gmail.com>
2024-02-10 21:35:51 +0800

    chore: pass style check

--- src/lpsim/server/patch/v44/balance/vourukashas_glow_4_4.py
+++ src/lpsim/server/patch/v44/balance/vourukashas_glow_4_4.py
@@ -3,9 +3,7 @@ from typing import Dict, List, Literal
 from ....action import MakeDamageAction
 from ....event import RoundEndEventArguments
 from ....struct import Cost
-from ....patch.v43.cards.vourukashas_glow_4_3 import (
-    VourukashasGlow_4_3
-)
+from ....patch.v43.cards.vourukashas_glow_4_3 import VourukashasGlow_4_3
 from ....match import Match
 from .....utils import register_class, DescDictType
 
@@ -13,7 +11,7 @@ from .....utils import register_class, DescDictType
 class VourukashasGlow_4_4(VourukashasGlow_4_3):
     name: Literal["Vourukasha's Glow"]
     version: Literal["4.4"] = "4.4"
-    cost: Cost = Cost(same_dice_number = 1)
+    cost: Cost = Cost(same_dice_number=1)
 
     def event_handler_ROUND_END(
         self, desc: RoundEndEventArguments, match: Match
@@ -28,8 +26,8 @@ desc: Dict[str, DescDictType] = {
     "ARTIFACT/Vourukasha's Glow": {
         "descs": {
             "4.4": {
-                "en-US": "After this character takes DMG: Draw 1 card. (Once per Round)\nEnd Phase: If this character has triggered the effect \"Draw 1 card\" this round: heal the attached character for 1 HP.\n(A character can equip a maximum of 1 Artifact)",  # noqa: E501
-                "zh-CN": "角色受到伤害后：如果所附属角色为「出战角色」，则抓1张牌。（每回合1次）\n结束阶段：若本角色在回合内触发了抓1张牌的效果，治疗所附属角色1点。\n（角色最多装备1件「圣遗物」）"  # noqa: E501
+                "en-US": 'After this character takes DMG: Draw 1 card. (Once per Round)\nEnd Phase: If this character has triggered the effect "Draw 1 card" this round: heal the attached character for 1 HP.\n(A character can equip a maximum of 1 Artifact)',  # noqa: E501
+                "zh-CN": "角色受到伤害后：如果所附属角色为「出战角色」，则抓1张牌。（每回合1次）\n结束阶段：若本角色在回合内触发了抓1张牌的效果，治疗所附属角色1点。\n（角色最多装备1件「圣遗物」）",  # noqa: E501
             }
         },
     },
--- src/lpsim/server/patch/v44/cards/jeht_4_4.py
+++ src/lpsim/server/patch/v44/cards/jeht_4_4.py
@@ -6,12 +6,8 @@ from typing import Dict, List, Literal
 from pydantic import PrivateAttr
 from ....match import Match
 from ....action import Actions, CreateDiceAction, RemoveObjectAction
-from ....event import (
-    RemoveObjectEventArguments, SkillEndEventArguments
-)
-from ....consts import (
-    DieColor, IconType, ObjectPositionType, SkillType
-)
+from ....event import RemoveObjectEventArguments, SkillEndEventArguments
+from ....consts import DieColor, IconType, ObjectPositionType, SkillType
 from ....card.support.companions import CompanionBase
 from ....struct import Cost
 from .....utils.class_registry import register_class
@@ -19,20 +15,22 @@ from .....utils.desc_registry import DescDictType
 
 
 class Jeht_4_4(CompanionBase):
-    name: Literal['Jeht']
-    version: Literal['4.4'] = '4.4'
+    name: Literal["Jeht"]
+    version: Literal["4.4"] = "4.4"
     usage: int = 0
     _max_usage: int = PrivateAttr(6)
     icon_type: Literal[IconType.TIMESTATE] = IconType.TIMESTATE
-    cost: Cost = Cost(any_dice_number = 2)
+    cost: Cost = Cost(any_dice_number=2)
 
     def event_handler_REMOVE_OBJECT(
         self, event: RemoveObjectEventArguments, match: Match
     ) -> List[Actions]:
         if not self.position.check_position_valid(
-            event.action.object_position, match, player_idx_same = True,
-            target_area = ObjectPositionType.SUPPORT,
-            source_area = ObjectPositionType.SUPPORT
+            event.action.object_position,
+            match,
+            player_idx_same=True,
+            target_area=ObjectPositionType.SUPPORT,
+            source_area=ObjectPositionType.SUPPORT,
         ):
             # not self or source or target not in support zone
             return []
@@ -44,8 +42,10 @@ class Jeht_4_4(CompanionBase):
         self, event: SkillEndEventArguments, match: Match
     ) -> List[CreateDiceAction | RemoveObjectAction]:
         if not self.position.check_position_valid(
-            event.action.position, match, player_idx_same = True,
-            target_area = ObjectPositionType.SKILL
+            event.action.position,
+            match,
+            player_idx_same=True,
+            target_area=ObjectPositionType.SKILL,
         ):
             # not self use skill
             return []
@@ -58,30 +58,25 @@ class Jeht_4_4(CompanionBase):
         # discard self and create dice
         return [
             CreateDiceAction(
-                player_idx = self.position.player_idx,
-                number = self.usage - 2,
-                color = DieColor.OMNI
+                player_idx=self.position.player_idx,
+                number=self.usage - 2,
+                color=DieColor.OMNI,
             ),
-            RemoveObjectAction(
-                object_position = self.position
-            )
+            RemoveObjectAction(object_position=self.position),
         ]
 
 
 desc: Dict[str, DescDictType] = {
     "SUPPORT/Jeht": {
-        "names": {
-            "en-US": "Jeht",
-            "zh-CN": "婕德"
-        },
+        "names": {"en-US": "Jeht", "zh-CN": "婕德"},
         "descs": {
             "4.4": {
                 "en-US": "This card will record the number of cards discarded from your Support Zone during this match as Sophistication points. (Max 6 points)\nAfter your characters use an Elemental Burst: If this card has recorded at least 5 Sophistication points, discard this card and generate Omni Element equal to the number of Sophistication points minus 2.",  # noqa: E501
-                "zh-CN": "此牌会记录本场对局中我方支援区弃置卡牌的数量，称为「阅历」。（最多6点）\n我方角色使用「元素爆发」后：如果「阅历」至少为5，则弃置此牌，生成「阅历」-2数量的万能元素。"  # noqa: E501
+                "zh-CN": "此牌会记录本场对局中我方支援区弃置卡牌的数量，称为「阅历」。（最多6点）\n我方角色使用「元素爆发」后：如果「阅历」至少为5，则弃置此牌，生成「阅历」-2数量的万能元素。",  # noqa: E501
             }
         },
         "image_path": "https://api.ambr.top/assets/UI/gcg/UI_Gcg_CardFace_Assist_NPC_Jeht.png",  # noqa: E501
-        "id": 322022
+        "id": 322022,
     },
 }
 
